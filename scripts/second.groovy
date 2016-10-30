ef37839eba78462bba547a33c14653bb58af2a80
Mike Hanafey <mike.hanafey@gmail.com>
2016-10-29 17:35:24 -0700

    Small step forward.

--- scripts/second.groovy
+++ scripts/second.groovy
@@ -1,17 +1,30 @@
 import java.sql.*
 import groovy.sql.Sql
 import groovy.util.CliBuilder
-import oracle.jdbc.pool.OracleDataSource;
+import oracle.jdbc.pool.OracleDataSource
 
 
 def cli = new CliBuilder(usage: 'This is my command.')
-cli.u(longOpt: 'user', args:1, argName:'user', 'Oracle username.')
-cli.p(longOpt: 'pw', args:1, argName:'pw', 'Oracle password.')
-cli.c(longOpt: 'cmd', args:1, argName:'cmd', 'Command, one of a|b|c')
+cli.u(longOpt: 'user', args: 1, argName: 'user', 'Oracle username.')
+cli.p(longOpt: 'pw', args: 1, argName: 'pw', 'Oracle password.')
+cli.c(longOpt: 'cmd', args: 1, argName: 'cmd', 'Command, one of a|b|c')
 def options = cli.parse(args)
 
-def sql = Sql.newInstance("jdbc:oracle:thin:@//esoravm4.es.dupont.com:1521/appspd", "sa", "sa")
+def queries = [
+        a: '''SELECT COUNT(*) 
+FROM ASSOCIATION_TEST
+'''
+]
 
-cli.usage()
+if (options.user && options.pw && options.cmd && queries[options.cmd]) {
+    cli:
+    usage()
+    exit 1
+}
 
-println "User: ${options.user}, PW: ${options.pw}, Cmd: ${options.cmd}"
+def sql = Sql.newInstance("jdbc:oracle:thin:@//esoravm4.es.dupont.com:1521/appspd", options.user, options.pw)
+
+
+sql.eachRow(queries[(String) (options.cmd)]) { row ->
+    println "Result is: ${row[1]}"
+}
